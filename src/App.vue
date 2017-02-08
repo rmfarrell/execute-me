7d1a182bcb4bbbc86dd24529a6525d919bee28db
Ryan <rmfarrell@gmail.com>
2017-02-07 23:09:07 -0500

    interface changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
 <template lang="pug">
 #app
+  form#image-selector(
+    onSubmit="return false"
+    @submit="createAnimation()",
+  )
+    .file-input.left(v-if="!leftImage.changed")
+      label="Drop an image here."
+      input(
+        type="file",
+        accept="image/*",
+        @change="submitImage(leftImage, $event)"
+      )
+    .image-indicator.left(v-if="leftImage.changed")
+      img(:src="leftImage.src")
+
+    .file-input.right(v-if="!rightImage.changed")
+      label="...And here."
+      input(
+        type="file",
+        accept="image/*",
+        @change="submitImage(rightImage, $event)"
+      )
+    .image-indicator.right(v-if="rightImage.changed")
+      img(:src="rightImage.src")
+
+    input(
+      type="submit",
+      value="Fabulous. Terrific. Sign it.",
+      :class="canSubmit ? '' : 'disabled' "
+    )
   virtual-canvas(
     :height="height",
     :width="width",
@@ -8,7 +37,10 @@
     :gif="gif",
     :frame-data="currentFrameData"
   )
-  img(:src="src")
+  #modal(:class="isProcessing ? '' : 'disabled'")
+    .loading-eagle
+    p.loading-indicator Loading...
+    img(:src="src")
 </template>
 
 <script>
@@ -21,6 +53,7 @@ export default {
     return {
       height: 648,
       width: 746,
+      isProcessing: false,
       gif: new window.GIF({
         workers: 4,
         quality: 2,
@@ -29,13 +62,15 @@ export default {
       }),
       leftImage: {
         loaded: false,
-        src: './static/img/che.jpg',
-        img: new Image()
+        src: './static/img/transparent.png',
+        img: new Image(),
+        changed: false
       },
       rightImage: {
         loaded: false,
-        src: './static/img/che.jpg',
-        img: new Image()
+        src: './static/img/transparent.png',
+        img: new Image(),
+        changed: false
       },
       src: '',
       currentFrame: 0,
@@ -49,9 +84,24 @@ export default {
     render () {
       this.nextFrame()
       if (this.isNextFrame()) {
+        console.log('render')
         this.$bus.$emit('render-frame', this.currentFrameData)
       } else {
         this.done()
+      }
+    },
+    submitImage (imgObject, ev) {
+      let f = ev.target.files[0]
+      let u = window.URL.createObjectURL(f)
+      imgObject.changed = true
+      imgObject.src = u
+      imgObject.img.src = u
+      this.loadImage(imgObject)
+    },
+    loadImage (imgObject) {
+      imgObject.loaded = false
+      imgObject.img.onload = () => {
+        imgObject.loaded = true
       }
     },
     nextFrame () {
@@ -61,30 +111,28 @@ export default {
     isNextFrame () {
       return typeof this.currentFrameData !== 'undefined'
     },
-    loadImages () {
-      this.leftImage.img.src = this.leftImage.src
-      this.rightImage.img.src = this.rightImage.src
-      this.leftImage.img.onload = () => {
-        this.leftImage.loaded = true
-        this.$emit('images-loaded')
+    loadPlaceHolders () {
+      this.loadImage(this.leftImage)
+      this.loadImage(this.rightImage)
+    },
+    createAnimation () {
+      if (this.rightImage.loaded && this.leftImage.loaded) {
+        this.$bus.$emit('render-frame', this.currentFrameData)
       }
-      this.rightImage.img.onload = () => {
-        this.rightImage.loaded = true
-        this.$emit('images-loaded')
-      }
+      this.isProcessing = true
     }
   },
   mounted () {
     this.gif.on('finished', (blob) => {
       this.src = URL.createObjectURL(blob)
     })
-    this.loadImages()
-    this.$on('images-loaded', () => {
-      if (this.rightImage.loaded && this.leftImage.loaded) {
-        this.$bus.$emit('render-frame', this.currentFrameData)
-      }
-    })
+    this.loadPlaceHolders()
     this.$bus.$on('frame-rendered', this.render)
+  },
+  computed: {
+    canSubmit () {
+      return (this.leftImage.changed && this.rightImage.changed)
+    }
   },
   components: {
     virtualCanvas
